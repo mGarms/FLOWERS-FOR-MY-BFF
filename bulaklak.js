af0832dfd7946f2f6b4f1d08fb428129296cffbe
mGarms <garemrem@gmail.com>
2025-02-21 10:48:40 +0000

    Update bulaklak.js

--- bulaklak.js
+++ bulaklak.js
@@ -2,7 +2,7 @@ onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
   
-      const titles = ('FLOWERS FOR U, KUPAL!').split('')
+      const titles = ('FLOWERS FOR U, KOGKOG!').split('')
       const titleElement = document.getElementById('title');
       let index = 0;
   
